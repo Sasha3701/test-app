f933024071ab6f8e97bfd9e963b61d3db93adf7d
Aleksandr Irhin <workirhin@yandex.ru>
2022-06-28 15:38:33 +0300

    Fixed modal

--- layout/index.js
+++ layout/index.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import { Header } from "../components";
 import styles from "./layout.module.scss";
 
 export const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Тестовое задание</title>
+      </Head>
       <Header />
       <main className={styles.main}>{children}</main>
     </>
